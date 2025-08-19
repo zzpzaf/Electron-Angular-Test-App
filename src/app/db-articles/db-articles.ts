e9626c44fd0662a07fcf3bb3aee45c4979f1efe8
Panos Zafeiropoulos <zzpzaf@gmail.com>
2025-08-19 10:44:38 +0300

    upd: Initial implementation of the NzLayoutModule / nz-layout in the DbArticles Component. A 3-area sider is used: (sider-header, sider-menu, and sider-footer). The middle-area (sider-menu) is going to filled with Category items fetched from the Main DB, but in this implementation, it shows just menu test items.

--- src/app/db-articles/db-articles.ts
+++ src/app/db-articles/db-articles.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 
 @Component({
   selector: 'app-db-articles',
-  imports: [],
+  imports: [
+    FormsModule, 
+    NzLayoutModule,
+    NzMenuModule,
+    NzCheckboxModule,
+    NzButtonModule,
+  ],
   templateUrl: './db-articles.html',
   styleUrl: './db-articles.scss'
 })
 export class DbArticles {
+
+  public rootOnly: boolean = true;
+
+  public isOnlyRootToggle() {
+    // this.rootOnly = !this.rootOnly;
+    console.log('>===>> Root Categories Only? ',this.rootOnly);
+  }
 
 }
